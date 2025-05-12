0724a50e7ed1b8ecb0a753e126de017071835261
hayden.rear <hayden.rear@gmail.com>
2025-05-12 17:06:09 -0400

    updating

--- docker/build.sh
+++ docker/build.sh
@@ -3,22 +3,24 @@
 echo "hello"
 
 
-#rm -rf build/ || true
-#mkdir -p build/resources || true
-#cp ../resources/application.yml build/application-docker.yml
-#cp ../resources/application-test.yml build/application-test.yml
-##sed -i -e s/{{gemini_api_key}}/"${GEMINI_API_KEY}"/g build/application-docker.yml
-#
-#cp -r ../src ./build/src
-#cp -r ../test ./build/test
-#cp ../pyproject.toml ./build
-#cp ../uv.lock ./build
-#cp ../.python-version ./build
-#
-#cp build/application-docker.yml ./build/resources/application.yml
-#cp build/application-test.yml ./build/resources/application-test.yml
-#cp ../docker.env ./build/.env
-#cp ../requirements.txt ./requirements.txt
+rm -rf build/ || true
+mkdir -p build/resources || true
+cp ../resources/application.yml build/application-docker.yml
+cp ../resources/application-test.yml build/application-test.yml
+#sed -i -e s/{{gemini_api_key}}/"${GEMINI_API_KEY}"/g build/application-docker.yml
+
+cp -r ../src ./build/src
+cp -r ../test ./build/test
+cp ../pyproject.toml ./build
+cp ../uv.lock ./build
+cp ../.python-version ./build
+
+cp build/application-docker.yml ./build/resources/application.yml
+cp build/application-test.yml ./build/resources/application-test.yml
+cp ../docker.env ./build/.env
+cp ../requirements.txt ./requirements.txt
+
+docker rm mcp/cdc_agents_test || true
 
 docker build . -f Dockerfile_testmcp -t mcp/cdc_agents_test
 
