b0769286589c2ac2b58e46ca5e1a97a90f95ee76
hayden.rear <hayden.rear@gmail.com>
2025-05-15 18:19:24 -0400

    move for code complete

--- src/cdc_agents/main/run_cdc_agents.py
+++ src/cdc_agents/main/run_cdc_agents.py
@@ -2,6 +2,8 @@ from cdc_agents.agent.agent_server import AgentServerRunner
 from python_di.configs.app import boot_application
 import os
 
+os.environ['SPRING_PROFILES_ACTIVE'] = 'a2a'
+
 @boot_application(root_dir_cls=AgentServerRunner)
 class CdcAgentsApplication:
     pass
--- src/cdc_agents/mcp_client/mcp_client_main.py
+++ src/cdc_agents/mcp_client/mcp_client_main.py
@@ -1,5 +1,6 @@
 import warnings
 
+import os
 from python_util.logger.log_level import LogLevel, LogLevelFacade
 
 from python_di.configs.app import boot_application
@@ -8,6 +9,7 @@ from cdc_agents.mcp_client.cdc_agents_mcp import CdcMcpAgents
 warnings.warn = lambda *args, **kwargs: None
 
 LogLevel.set_log_level(LogLevelFacade.Ctx)
+os.environ['SPRING_PROFILES_ACTIVE'] = 'mcp'
 
 @boot_application(root_dir_cls=CdcMcpAgents)
 class CdcAgentsApplication:
